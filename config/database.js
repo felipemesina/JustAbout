1e45f730ac6d08b2a516da500e935e1f784632c6
Felipe <felipejmesina@gmail.com>
2018-07-06 23:10:59 -0700

    reconfigured mongo uri

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const crypto = require("crypto").randomBytes(256).toString("hex");
 
 module.exports =  {
-  uri: "mongodb://54.193.59.89:27017/JustAbout",
+  uri: "mongodb://localhost:27017/JustAbout",
   secret: crypto
 }
