71d3504f28e014b35a0e80c6223e458088501373
Felipe <felipejmesina@gmail.com>
2018-07-31 21:38:31 -0700

    changed mongo uri

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const crypto = require("crypto").randomBytes(256).toString("hex");
 
 module.exports =  {
-  uri: "mongodb://ec2-13-56-224-216.us-west-1.compute.amazonaws.com:27017/JustAbout",
+  uri: "mongodb://ec2-13-57-226-110.us-west-1.compute.amazonaws.com:27017/JustAbout",
   secret: crypto
 }
