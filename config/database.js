73a5c2146525a6069c4cd77a13d7b3b01bb36655
Felipe <felipejmesina@gmail.com>
2018-08-18 09:16:08 -0700

    reconfig'd database uri

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const crypto = require("crypto").randomBytes(256).toString("hex");
 
 module.exports =  {
-  uri: "mongodb://ec2-54-219-143-86.us-west-1.compute.amazonaws.com:27017/justAbout",
+  uri: "mongodb://felipemesina:Incarcasswetrust17@ec2-54-219-143-86.us-west-1.compute.amazonaws.com:27017/justAbout",
   secret: crypto
 }
